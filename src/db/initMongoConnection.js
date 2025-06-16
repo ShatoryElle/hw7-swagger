efdbeec7658b4b1cbf969d9008164b6f08e0d4f2
Vita <Shatory@gmail.com>
2025-06-16 14:41:08 +0300

    swag

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -1,13 +1,18 @@
 import mongoose from 'mongoose';
-import { getEnvVar } from '../utils/getEnvVar.js';  
+import { getEnvVar } from '../utils/getEnvVar.js';
 
 export const initMongoConnection = async () => {
   try {
     const mongoUri = getEnvVar('MONGODB_URL');
 
+    console.log('MONGODB_URL from env:', mongoUri);
+    
     await mongoose.connect(mongoUri, {
       appName: 'homeWork',
     });
+
+
+
 
     console.log('Mongo connection successfully established!');
   } catch (e) {
