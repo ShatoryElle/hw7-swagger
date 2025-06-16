a7255dc0c1b53a0d5aebdbb45ee56b9cf542f060
Vita <Shatory@gmail.com>
2025-06-16 15:03:36 +0300

    swagge git commit -m swagge

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -1,24 +1,18 @@
 import mongoose from 'mongoose';
+import { getEnvVar } from '../utils/getEnvVar.js';
 
 export const initMongoConnection = async () => {
   try {
-    const mongoUri = process.env.MONGODB_URL;
-
+    const mongoUri = getEnvVar('MONGODB_URL');
     console.log('MONGODB_URL from env:', mongoUri);
-
-    if (!mongoUri) {
-      throw new Error('MONGODB_URL is not defined in environment variables');
-    }
 
     await mongoose.connect(mongoUri, {
       appName: 'homeWork',
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
     });
 
     console.log('Mongo connection successfully established!');
   } catch (e) {
-    console.error('Error while setting up mongo connection', e);
+    console.log('Error while setting up mongo connection', e);
     throw e;
   }
 };
